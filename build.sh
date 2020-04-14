9828f950ae9b95a406cc2b3804b13fdb2455f68e
cesar <cesar@hearthapp.io>
2020-04-13 23:41:36 -0400

    Initialize fork

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-
+# clean up any older data
 rm -rf swot.zip
 rm -rf swot-build
 
+# fetch the latest version of swot data
 wget https://codeload.github.com/JetBrains/swot/zip/master -O swot.zip
 mkdir swot-build
 unzip swot.zip -d swot-build
 
+# generate updated swot mapping
+npm install --dev
 node build.js
-
-npm run build
-
-npm run test
+browserify -s swot index.js | uglifyjs -c -m > swot-simple.js
